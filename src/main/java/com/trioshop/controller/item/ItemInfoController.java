a0e790cdc1419a512f137d9ebe89ebaa11df15da
SWY <swypawn@gmail.com>
2024-05-20 21:35:12 +0900

    /Fix : 오타수정, test controller 및 dao제거

--- src/main/java/com/trioshop/controller/item/ItemInfoController.java
+++ src/main/java/com/trioshop/controller/item/ItemInfoController.java
@@ -4,6 +4,7 @@ import com.trioshop.model.dto.item.ItemInfoByUser;
 import com.trioshop.repository.dao.item.ItemInfoDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,8 +12,19 @@ import java.util.List;
 
 @Controller
 public class ItemInfoController {
+
     @Autowired
     ItemInfoDao itemInfoDao;
+    @RequestMapping("/")
+    public ModelAndView userList(Model model) {
+        System.out.println("homePage");
+        ModelAndView mv = new ModelAndView();
+
+        List<ItemInfoByUser> itemList = itemInfoDao.findAllItem();
+        mv.addObject("itemList", itemList);
+        mv.setViewName("/etc/homePage");
+        return mv;
+    }
     @RequestMapping("/itemList")//상품 전체리스트 페이지로
     public ModelAndView itemListPage() {
 
