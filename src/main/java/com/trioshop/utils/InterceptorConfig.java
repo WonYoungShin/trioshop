b3ffb8313562e51bfa335c7d34214a0741acde1c
wonhyeong <kkgg0523@gmail.com>
2024-06-03 14:39:26 +0900

    Feat: Filter -> interceptor 변경 - - Feat: 관리자 인터셉터 추가

---
+++ src/main/java/com/trioshop/utils/InterceptorConfig.java
@@ -0,0 +1,25 @@
+package com.trioshop.utils;
+
+import com.trioshop.interceptor.AdminCheckInterceptor;
+import com.trioshop.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/","/login", "/css/**", "/images/*", "/itemInfo" ,"/itemList",
+                        "/SearchItems", "/cart", "/item/**" , "/join", "/findId",
+                        "/findPw", "/guestLogin","/logout", "/*.ico","/error");
+
+        registry.addInterceptor(new AdminCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/trioAdmin/**")
+                .excludePathPatterns("/","/login", "/css/**", "/images/*","/*.ico");
+    }
+}
